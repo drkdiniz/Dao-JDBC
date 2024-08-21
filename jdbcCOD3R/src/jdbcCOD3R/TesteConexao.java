dc2830aa04328b320150d7a7e1fbdce8881b5b5a
Derek Diniz <drk.diniz@gmail.com>
2024-08-21 08:49:00 -0300

    Curso cod3r

---
+++ jdbcCOD3R/src/jdbcCOD3R/TesteConexao.java
@@ -0,0 +1,19 @@
+package jdbcCOD3R;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+	public static void main(String[] args) throws SQLException{
+		
+		String url = "jdbc:mysql://localhost";
+		String usuario = "derek";
+		String senha = "acido11";
+		
+		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		
+		System.out.println("Conexão efetuada com sucesso!");
+		conexao.close();
+	}
+}
